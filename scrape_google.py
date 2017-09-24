8175d531f7dcec9a2edf0931e0aa552cf2d49a91
Ali Mahmoud <ali.mahmoud.engineer@gmail.com>
2017-09-24 23:24:47 +0200

    Add scrape func

--- scrape_google.py
+++ scrape_google.py
@@ -1,16 +1,18 @@
 import requests
 from selenium import webdriver
-
-from bs4 import BeautifulSoup
-from time import sleep
-import json
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from bs4 import BeautifulSoup
+import json
+import time
 
-chrome_driver = '/media/bluehat7/Ali Only/Freelancing/google jobs scraper/chromedriver'
+
+# Path of chrome driver
+chrome_driver = '/media/bluehat7/Ali Only/Freelancing/google jobs scraper/chrome_driver/chromedriver'
+
+# Creates a new instance of the chrome driver
 browser = webdriver.Chrome(executable_path=chrome_driver)
 
 data = {}
@@ -18,123 +20,141 @@ jobs = []
 total = 0
 
 
-def prepare():
-    """Prepare a website url"""
-    url = 'https://careers.google.com/jobs'
+def scrape():
+    """Prepare a website url and scrape all pages"""
+
+    # Prepare the URL
+    base_url = 'https://careers.google.com/jobs'
     params = {
         't': 'sq',
+        'li': '20',
         'st': '0',
         'jlo': 'all'
     }
+    result = requests.get(url=base_url, params=params)
+    url = result.url.replace('?', '#')
 
-    result = requests.get(url, params)
-    return result.url.replace('?', '#')
+    # Scrape all pages
+    start = time.time()
+    print('Start scraping all google jobs ...')
+    print('Please, Do not close chrome driver. '
+          'It will be closed automatically after finished.')
+    print('This process maybe take more than 10 minutes')
+    count_pages = 280
+    while True:
+        website_url = url.replace('st=0', 'st={}'.format(count_pages))
+        try:
+            parse(website_url)
+        except TimeoutException:
+            browser.close()
+            print('All data successfully scraped!')
+            end = time.time()
+            print('Time: {} minutes'.format(round((end - start) / 60), 1))
+            break
+        count_pages += 20
+
+    # Store all jobs data and total count
+    data['total'] = total
+    data['jobs'] = jobs
+
+    return json.dumps(data)
 
 
-def parse(website):
-    """Parse google jobs data"""
+def parse(website_url):
+    """Parse main jobs page and get all jobs URLs"""
     global total
-    browser.get(website)
 
-    # sleep(20)
-    WebDriverWait(browser, 30).until(
+    # Open first URL
+    browser.get(website_url)
+
+    # Waite or sleep till all page data loaded
+    WebDriverWait(browser, 20).until(
         EC.presence_of_all_elements_located((By.CLASS_NAME, 'GXRRIBB-e-G'))
     )
 
     body = browser.page_source
-    # browser.close()
 
+    # Parse page html to extract jobs info
     soup = BeautifulSoup(body, 'html.parser')
     jobs_content = soup.select('.GXRRIBB-e-G')
-    print(len(jobs_content))
+
+    # Store the count of jobs located in this page
     total += len(jobs_content)
 
+    # Get all jobs links (URLs) form jobs info (jobs_content variable)
     jobs_urls = []
     for job in jobs_content:
         job_header = job.select('h2 a')[0]
-        google = job.select_one('div.sr-content div.summary .secondary-text').get_text()
+
+        # Make sure that job related to Google company
+        google = job.select_one(
+            'div.sr-content div.summary .secondary-text').get_text()
         if google == 'Google':
-            job_link = 'https://careers.google.com/jobs' + job_header.get('href')
+            job_link = 'https://careers.google.com/jobs'\
+                       + job_header.get('href')
             jobs_urls.append(job_link)
 
     parse_jobs(jobs_urls)
 
 
 def parse_jobs(jobs_urls):
-    """Parse jobs by its urls"""
+    """Parse all jobs data"""
     global jobs
-    jobs_html = []
 
-    for i in range(len(jobs_urls)):
-        browser.execute_script("window.open('{}', 'new_window')".format(jobs_urls[i]))
+    # Open jobs URLs one by one to get page html
+    jobs_html = []
+    for url in jobs_urls:
+        browser.execute_script("window.open('{}', 'new_window')".format(url))
         browser.switch_to.window(browser.window_handles[1])
 
-        if i == 0:
-            WebDriverWait(browser, 30).until(
+        # Waite till all page data loaded
+        try:
+            WebDriverWait(browser, 20).until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.secondary-text'))
             )
-        else:
-            WebDriverWait(browser, 30).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.description-section p'))
-            )
+        except TimeoutException:
+            browser.close()
+            browser.switch_to.window(browser.window_handles[0])
+            continue
+        '''
+        WebDriverWait(browser, 20).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.description-section p'))
+        )
+        '''
 
         jobs_html.append(browser.page_source)
+
+        # Close the current windows and switch to the main window
         browser.close()
         browser.switch_to.window(browser.window_handles[0])
 
-    job_dict = {}
+    # Parse all pages html to get jobs data
     jobs_list = []
     for job in jobs_html:
         soup = BeautifulSoup(job, 'html.parser')
         job_id = soup.find('div', attrs={'itemtype': 'http://schema.org/JobPosting'}).get('id')
         job_title = soup.select_one('div.card-company-job-details > h1 a.title.text').get_text()
         location = soup.select_one('div.card-company-job-details .details-panel > a').get_text()
-
         desc = soup.select_one('div.detail-item .description-section.text.with-benefits').get_text()
         resp_qual = soup.select('div.detail-item .description-section .GXRRIBB-S-c .description-content')
         resp = resp_qual[0].get_text()
         qual = resp_qual[1].get_text()
-        '''
-        print(job_id)
-        print(job_title)
-        print(location)
-        print(desc)
-        print(resp)
-        print(qual)
-        print('\n============================================\n')
-        '''
-        job_dict['job_id'] = job_id
-        job_dict['job_title'] = job_title
-        job_dict['location'] = location
-        job_dict['introduction'] = desc
-        job_dict['responsibilities'] = resp
-        job_dict['qualifications'] = qual
-        jobs_list.append(job_dict.copy())
-        job_dict.clear()
 
+        job_dict = {
+            'job_id': job_id,
+            'job_title': job_title,
+            'job_location': location,
+            'introduction': desc,
+            'responsibilities': resp,
+            'qualifications': qual
+        }
+
+        jobs_list.append(job_dict)
+
+    # Store the current jobs list with all scraped lists before
     jobs += jobs_list
 
 
 if __name__ == '__main__':
-
-    # Scrape all pages
-    count_pages = 0
-    while True:
-        print(count_pages)
-        url = prepare()
-        url = url.replace('st=0', 'st={}'.format(count_pages))
-        print(url)
-        try:
-            parse(url)
-        except TimeoutException:
-            browser.close()
-            break
-
-        count_pages += 20
-
-    browser.close()
-    data['total'] = total
-    data['jobs'] = jobs
-    data_json = json.dumps(data)
+    data_json = scrape()
     print(data_json)
-
